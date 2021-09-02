cd5a32da62065489abd7b62809dc392d38668364
Eric Wong <erichwlwong@gmail.com>
2021-09-02 05:59:00 -0700

    add handlers

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-func HandleRequests() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func HandleRequests() {
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", SignupUser).Methods("POST")
+	router.HandleFunc("/login", LoginUser).Methods("POST")
+	router.HandleFunc("/user", AddBalance).Methods("PUT")
+	router.HandleFunc("/user", BuyShare).Methods("POST")
+	router.HandleFunc("/user", SellShare).Methods("POST")
+	router.HandleFunc("/user", Subscribe).Methods("POST")
+	router.HandleFunc("/user", getPortfolio).Methods("GET")
+
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
 
 func main() {
-
+	HandleRequests()
 }
