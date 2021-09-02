2ca92aceff583a5614c88d258d85858993b12ab3
Eric Wong <erichwlwong@gmail.com>
2021-09-02 06:36:06 -0700

    add database connection

--- main.go
+++ main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
+
+var db *pg.DB
 
 func HandleRequests() {
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", SignupUser).Methods("POST")
 	router.HandleFunc("/login", LoginUser).Methods("POST")
-	router.HandleFunc("/user", AddBalance).Methods("PUT")
 	router.HandleFunc("/user", BuyShare).Methods("POST")
 	router.HandleFunc("/user", SellShare).Methods("POST")
 	router.HandleFunc("/user", Subscribe).Methods("POST")
+
+	router.HandleFunc("/user", AddBalance).Methods("PUT")
+
 	router.HandleFunc("/user", getPortfolio).Methods("GET")
 
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
 
 func main() {
+	//connect to db
+	db = pg.Connect(&pg.Options{
+		Addr:     "localhost:5432",
+		User:     "postgres",
+		Password: "postgres",
+		Database: "testdb",
+	})
+	defer db.Close()
+
+	//creates table users
+	err := db.Model((*User)(nil)).CreateTable(&orm.CreateTableOptions{})
+	if err != nil {
+
+		fmt.Println("Table exists")
+	}
+
 	HandleRequests()
 }
