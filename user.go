cfaf40edfa9261388ae461e27330add49c50252e
Eric Wong <erichwlwong@gmail.com>
2021-09-02 05:55:26 -0700

    add structs to user

--- user.go
+++ user.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	Email        string   `json:"email" pg:",pk"`
+	Password     string   `json:"password,omitempty"`
+	FirstName    string   `json:"firstName"`
+	LastName     string   `json:"lastName"`
+	Balance      int64    `json:"balance"`
+	Subscription []string `json:"subscription"`
+	Shares       []Share  `json:"shares"`
+}
+
+type Share struct {
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+}
+
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func SignupUser(w http.ResponseWriter, r *http.Request) {
+}
+
 func AddBalance() {
 
 }
